98bc34e6992d8658a7dcd08eb9518fb31abc40d9
Vladyslav <vlad.betcun@gmail.com>
2021-07-26 18:41:04 +0300

    test: increase code coverage to 100%, closes #38 (#47)

--- test.js
+++ test.js
@@ -2,8 +2,10 @@
 
 const { beforeEach, test } = require('tap')
 const { performance } = require('perf_hooks')
+const { Transform } = require('stream')
 const Fastify = require('fastify')
 const Stats = require('.')
+const fakeTimer = require('@sinonjs/fake-timers')
 
 beforeEach(async () => {
   performance.clearMarks()
@@ -16,6 +18,30 @@ test('produces some stats', async (t) => {
   t.teardown(fastify.close.bind(fastify))
 
   fastify.get('/', async () => {
+    return { hello: 'world' }
+  })
+
+  await fastify.inject({
+    url: '/'
+  })
+
+  const measurements = fastify.measurements()
+  const nums = measurements.GET['/']
+  t.ok(nums[0] >= 0)
+  t.ok(nums.length === 1)
+})
+
+test('has no conflicts with custom measures', async (t) => {
+  const fastify = Fastify()
+  fastify.register(Stats)
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', async () => {
+    performance.mark('A')
+    performance.mark('B')
+    performance.measure('A to B', 'A', 'B')
+
     return { hello: 'world' }
   })
 
@@ -156,4 +182,84 @@ test('produces stats for multiple methods', async (t) => {
   const posts = measurements.POST['/']
   t.ok(posts[0] >= 0)
   t.ok(posts.length === 1)
+})
+
+test('produces stats for multiple routes of method', async (t) => {
+  const fastify = Fastify()
+  fastify.register(Stats)
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/first', async () => {
+    return { hello: 'world' }
+  })
+
+  fastify.get('/second', async () => {
+    return { hello: 'world' }
+  })
+
+  await fastify.inject({
+    url: '/first'
+  })
+
+  await fastify.inject({
+    url: '/first'
+  })
+
+  await fastify.inject({
+    url: '/second'
+  })
+
+  await fastify.inject({
+    url: '/second'
+  })
+
+  const stats = fastify.stats()
+  Object.values(stats.GET).forEach(nums => {
+    t.ok(nums.mean >= 0)
+    t.ok(nums.mode >= 0)
+    t.ok(nums.median >= 0)
+    t.ok(nums.max >= 0)
+    t.ok(nums.min >= 0)
+    t.ok(nums.sd >= 0)
+  })
+})
+
+test('logs stats every 30 sec', async (t) => {
+  const clock = fakeTimer.install()
+
+  const stream = new Transform({
+    objectMode: true,
+    transform: (chunk, enc, cb) => cb(null, JSON.parse(chunk))
+  })
+
+  const fastify = Fastify({ logger: { stream: stream, level: 'info' } })
+  fastify.register(Stats)
+
+  t.teardown(() => {
+    clock.uninstall()
+    fastify.close()
+  })
+
+  fastify.get('/', async () => {
+    return { hello: 'world' }
+  })
+
+  const matches = [
+    { msg: /incoming request/ },
+    { msg: /request completed/ },
+    { msg: /routes stats/ }
+  ]
+
+  let i = 0
+  stream.on('data', line => {
+    t.match(line, matches[i])
+    i += 1
+  })
+
+  await fastify.inject({
+    url: '/'
+  })
+
+  clock.tick('00:35')
 })
