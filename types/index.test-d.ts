fae41f0af7b524f8e152d6ad3605c15ebd150176
uzlopak <aras.abbasi@googlemail.com>
2022-08-08 02:41:55 +0200

    add typings

---
+++ types/index.test-d.ts
@@ -0,0 +1,26 @@
+import Fastify from 'fastify'
+import { expectAssignable, expectError, expectType } from 'tsd'
+import fastifyRoutesStats from '..'
+
+const fastify = Fastify();
+
+fastify.register(fastifyRoutesStats, { printInterval: 1000 })
+expectError(fastify.register(fastifyRoutesStats, { printInterval: 'a' }))
+
+expectAssignable<Function>(fastify.measurements)
+expectAssignable<Function>(fastify.stats)
+
+
+fastify.get('/stats', function (req, reply) {
+  expectAssignable<Function>(this.measurements)
+  expectAssignable<Function>(this.stats)
+})
+
+expectType<Array<number>>(fastify.measurements().GET!['test'])
+expectAssignable<Record<string, {}> | undefined>(fastify.stats().GET)
+expectType<number>(fastify.stats().GET!['/'].max)
+expectType<number>(fastify.stats().GET!['/'].mean)
+expectType<number>(fastify.stats().GET!['/'].median)
+expectType<number>(fastify.stats().GET!['/'].min)
+expectType<number>(fastify.stats().GET!['/'].mode)
+expectType<number>(fastify.stats().GET!['/'].sd)
