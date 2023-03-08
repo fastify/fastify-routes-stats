3eb9f3342a6ca24ebe29c70619354254431ab735
uzlopak <aras.abbasi@googlemail.com>
2023-03-08 16:18:28 +0100

    minor performance optimizations

--- index.js
+++ index.js
@@ -12,17 +12,20 @@ async function fastifyRoutesStats (fastify, opts) {
   let observedEntries = {}
   const obs = new PerformanceObserver((items) => {
     const fetchedItems = items.getEntries()
-    for (let i = 0; i < fetchedItems.length; i++) {
+    for (let i = 0, il = fetchedItems.length; i < il; ++i) {
       const e = fetchedItems[i]
-      if (e.name.indexOf(ROUTES) === 0) {
-        const method = e.name.split(':')[1].split('|')[0]
-        const route = e.name.split(':')[1].split('|')[1]
-        if (observedEntries[method] && observedEntries[method][route]) {
+      if (e.name.startsWith(ROUTES)) {
+        const [
+          method,
+          route
+        ] = e.name.split(':')[1].split('|')
+        if (
+          method in observedEntries &&
+          route in observedEntries[method]
+        ) {
           observedEntries[method][route].push(e.duration)
         } else {
-          if (!observedEntries[method]) {
-            observedEntries[method] = {}
-          }
+          method in observedEntries || (observedEntries[method] = {})
           observedEntries[method][route] = [e.duration]
         }
       }
@@ -39,10 +42,9 @@ async function fastifyRoutesStats (fastify, opts) {
   })
 
   fastify.addHook('onSend', function (request, reply, _, next) {
-    let routeId = request.raw.url
-    if (reply.context.config.statsId) {
-      routeId = reply.context.config.statsId
-    }
+    const routeId = reply.context.config.statsId
+      ? reply.context.config.statsId
+      : request.raw.url
 
     const id = request.raw.id
     performance.mark(ONSEND + id)
@@ -81,16 +83,14 @@ async function fastifyRoutesStats (fastify, opts) {
     const results = {}
 
     const methods = Object.keys(m)
-    for (let i = 0; i < methods.length; i++) {
+    for (let i = 0, il = methods.length; i < il; ++i) {
       const method = methods[i]
       const routes = Object.keys(m[method])
-      for (let j = 0; j < routes.length; j++) {
+      for (let j = 0, jl = routes.length; j < jl; ++j) {
         const route = routes[j]
         const s = summary(m[method][route])
 
-        if (!results[method]) {
-          results[method] = {}
-        }
+        method in results || (results[method] = {})
 
         results[method][route] = {
           mean: s.mean(),
